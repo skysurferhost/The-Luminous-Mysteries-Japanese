7197dc3fee84721fc1e0d97faaecacc880fd93ff
vuongskysurferstorage <vuongskysurferstorage1@gmail.com>
2026-04-25 14:13:58 -0400

    update

---
+++ data.js
@@ -0,0 +1,274 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan",
+      "name": "一、イエス、ヨルダン川で洗礼を受ける",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6649957818216627,
+          "pitch": 0.08677225450899329,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6319923911907424,
+          "pitch": -0.17290606185455815,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエス、ヨルダン川で洗礼を受ける</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">その時、イエスが、ガリラヤからヨルダン川のヨハネのところへ来られた。彼から洗礼(バプテスマ)を受けるためである。ところが、ヨハネは、それを思いとどまらせようとして言った。「私こそ、あなたから洗礼(バプテスマ)を受けるべきなのに、あなたが、私のところに来られたのですか。」しかし、イエスはお答えになった。「今はそうさせてもらいたい。すべてを正しく行うのは、我々にふさわしいことです。」そこで、ヨハネはイエスの言われるとおりにした。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">イエスは洗礼(バプテスマ)を受けると、すぐに水から上がられた。すると、天が開け、神の霊が鳩のようにご自分の上に降って来るのを御覧になった。そして、「これは私の愛する子、私の心に適う者」と言う声が、天から聞こえた。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(マタイ 3:13–17)<o:p></o:p></p></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-2-c-cha-gisu-d-tic-ci-cana",
+      "name": "二、イエス、カナの婚礼で最初のしるしを行う",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.20790684707299256,
+          "pitch": -0.008690813679965714,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        },
+        {
+          "yaw": 1.3359429840287653,
+          "pitch": 0.13937177954095148,
+          "rotation": 0,
+          "target": "0-1-c-cha-gisu-chu-php-ra-ti-sng-gioan"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.24373764674771792,
+          "pitch": -0.2789593316817669,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエス、カナの婚礼で最初のしるしを行う</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">三日目に、ガリラヤのカナで婚礼があって、イエスの母がそこにいた。イエスとその弟子たちも婚礼に招かれた。ぶどう酒がなくなってしまったとき、母がイエスに、「ぶどう酒がありません」と言った。イエスは母に言われた。「女よ、私とどんな関わりがあるのです。私の時はまだ来ていません。」母は召し使いたちに、「この方が言いつけるとおりにしてください」と言った。</div></span><o:p></o:p></p><p class=\"MsoNormal\"><br></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">そこには、ユダヤ人が清めに用いる石の水がめが六つ置いてあった。いずれも二ないし三メトレテス入りのものである。イエスが、「水がめに水をいっぱい入れなさい」と言われると、召し使いたちは、かめの縁まで水を満たした。イエスは、「さあ、それを汲んで、宴会の世話役のところへ持って行きなさい」と言われた。召し使いたちは運んで行った。世話役が水をなめてみると、ぶどう酒に変わっていた。それがどこから来たものなのか、分からなかったので――水を汲んだ召し使いたちは知っていたが――、世話役は花婿を呼んで、言った。「誰でも初めに良いぶどう酒を出し、酔いが回った頃に劣ったものを出すものですが、あなたは良いぶどう酒を今まで取っておかれました。」イエスは、この最初のしるしをガリラヤのカナで行って、その栄光を現された。それで、弟子たちはイエスを信じた。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(ヨハネ 2:1–11)</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi",
+      "name": "三、イエス、神の国の到来を告げ、人々を回心に招く",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05358848048561171,
+          "pitch": -0.17051613309329028,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        },
+        {
+          "yaw": -1.7029965550555701,
+          "pitch": 0.1012106739874703,
+          "rotation": 0,
+          "target": "1-2-c-cha-gisu-d-tic-ci-cana"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.004657148241202336,
+          "pitch": -0.4721606572718109,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエス、神の国の到来を告げ、人々を回心に招く</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">ヨハネが捕らえられた後、イエスはガリラヤへ行き、神の福音を宣べ伝えて、「時は満ち、神の国は近づいた。悔い改めて、福音を信じなさい」と言われた。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(マルコ 1:14–15)</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-4-c-cha-gisu-bin-hnh-trn-ni",
+      "name": "四、イエス、タボル山で栄光の姿を現す",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7953274170598537,
+          "pitch": 0.37459110494686243,
+          "rotation": 0,
+          "target": "4-5-c-cha-gisu-lp-b-tch-thnh-th"
+        },
+        {
+          "yaw": -1.748073222986461,
+          "pitch": -0.012033693567310877,
+          "rotation": 0,
+          "target": "2-3-c-cha-gisu-rao-ging-nc-tri-v-ku-gi-sm-hi"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.71832037522912,
+          "pitch": -0.1197861019002886,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエス、タボル山で栄光の姿を現す</span><o:p></o:p></p>",
+          "text":  "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">六日の後、イエスは、ペトロ、それにヤコブとその兄弟ヨハネだけを連れて、高い山に登られた。すると、彼らの目の前でイエスの姿が変わり、顔は太陽のように輝き、衣は光のように白くなった。見ると、モーセとエリヤが現れ、イエスと語り合っていた。ペトロが口を挟んでイエスに言った。「主よ、私たちがここにいるのは、すばらしいことです。お望みでしたら、ここに幕屋を三つ建てましょう。一つはあなたのため、一つはモーセのため、もう一つはエリヤのために。」ペトロがこう話しているうちに、光り輝く雲が彼らを覆った。すると、雲の中から、「これは私の愛する子、私の心に適う者。これに聞け」と言う声がした。弟子たちはこれを聞いてひれ伏し、非常に恐れた。イエスは近寄り、彼らに手を触れて言われた。「立ち上がりなさい。恐れることはない。」彼らが目を上げて見ると、イエスのほかには誰もいなかった。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(マタイ 17:1–8)</div>"
+        }
+      ]
+    },
+    {
+      "id": "4-5-c-cha-gisu-lp-b-tch-thnh-th",
+      "name": "五、イエス、最後の晩さんで聖体の秘跡を制定する",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7996773456601955,
+          "pitch": 0.0118117757142997,
+          "rotation": 0,
+          "target": "3-4-c-cha-gisu-bin-hnh-trn-ni"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.06865999545901147,
+          "pitch": -0.5026090379846924,
+          "title": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 115%;\">イエス、最後の晩さんで聖体の秘跡を制定する</span><o:p></o:p></p>",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">一同が食事をしているとき、イエスはパンを取り、祝福してそれを裂き、弟子たちに与えて言われた。「取って食べなさい。これは私の体である。」また、杯を取り、感謝を献げて彼らに与え、言われた。「皆、この杯から飲みなさい。これは、罪が赦されるように、多くの人のために流される、私の契約の血である。<o:p></o:p></span></p><p class=\"MsoNormal\"><span style=\"font-size: 16pt; line-height: 150%;\">(マタイ 26:26–28)</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
